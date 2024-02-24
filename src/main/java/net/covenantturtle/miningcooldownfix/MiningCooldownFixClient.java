5bcadb55f959f0d9865497c6ea32e5e6cbf21050
Timardo <33033989+Timardo@users.noreply.github.com>
2024-02-24 02:16:07 +0100

    Add info to chat

--- src/main/java/net/covenantturtle/miningcooldownfix/MiningCooldownFixClient.java
+++ src/main/java/net/covenantturtle/miningcooldownfix/MiningCooldownFixClient.java
@@ -1,9 +1,13 @@
 package net.covenantturtle.miningcooldownfix;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import net.covenantturtle.miningcooldownfix.config.MiningCooldownFixConfig;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.text.LiteralTextContent;
+import net.minecraft.text.MutableText;
 
 public class MiningCooldownFixClient implements ClientModInitializer {
     public static KeyBinding keybind;
@@ -14,6 +18,8 @@ public class MiningCooldownFixClient implements ClientModInitializer {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (keybind.wasPressed()) {
                 MiningCooldownFix.getConfig().enabled = !MiningCooldownFix.getConfig().enabled;
+                AutoConfig.getConfigHolder(MiningCooldownFixConfig.class).save();
+                client.player.sendMessage(MutableText.of(new LiteralTextContent("MiningCooldownFix " + (MiningCooldownFix.getConfig().enabled ? "enabled" : "disabled"))));
             }
         });
     }
